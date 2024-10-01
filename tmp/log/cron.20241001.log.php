cd2e0cb0aa70130f02808a70d6ca6575272fd2c7
kumarit84 <kumarit84@gmail.com>
2024-10-01 22:35:36 +0800

    rtt

--- tmp/log/cron.20241001.log.php
+++ tmp/log/cron.20241001.log.php
@@ -37,3 +37,77 @@ command: moduleName=compile&methodName=syncCompile.
 return : .
 output : 
 success
+22:30:29 task 8 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+22:30:29 task 9 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+22:30:29 task 10 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+22:30:29 task 13 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+22:30:29 task 14 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+22:30:29 task 15 executed,
+command: moduleName=mr&methodName=syncMR.
+return : .
+output : 
+success
+22:30:29 task 16 executed,
+command: moduleName=compile&methodName=syncCompile.
+return : .
+output : 
+success
+22:35:13 task 8 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+22:35:13 task 9 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+22:35:13 task 10 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+22:35:13 task 13 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+22:35:13 task 14 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+22:35:13 task 15 executed,
+command: moduleName=mr&methodName=syncMR.
+return : .
+output : 
+success
+22:35:13 task 16 executed,
+command: moduleName=compile&methodName=syncCompile.
+return : .
+output : 
+success
--- tmp/log/saas.20241001.log.php
+++ tmp/log/saas.20241001.log.php
@@ -1,3 +1,6 @@
 <?php die(); ?>20241001 22:13:38: misc-checkUpdate-9b2e4a6ffe4441c5e97b0d8c79b81295
 url:    https://api.zentao.pm/updater-getLatest-18.3-zentao-en-9b2e4a6ffe4441c5e97b0d8c79b81295.html
 results:[]
+20241001 22:33:56: misc-checkUpdate-9b2e4a6ffe4441c5e97b0d8c79b81295
+url:    https://api.zentao.pm/updater-getLatest-18.3-zentao-en-9b2e4a6ffe4441c5e97b0d8c79b81295.html
+results:[]
